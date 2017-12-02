459463b8a207884ae83679bb1af89260f4e67463
Safonov88 <safonovdmitr@gmail.com>
2017-12-02 19:01:20 +0300

    fix carrierwave.rb v3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,16 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_use_ssl_for_aws = true
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
+    config.fog_public     = true
+    config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
+    config.asset_host = 'https://s3.amazonaws.com/website'
+
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
-    config.fog_directory = ENV['S3_BUCKET_NAME']
   end
 end

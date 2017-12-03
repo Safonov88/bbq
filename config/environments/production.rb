37094004d82c0419913bff0517fa588a22f6d8d7
Safonov88 <safonovdmitr@gmail.com>
2017-12-03 14:33:13 +0300

    fix actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: 'bbqnew.heroku.com'}
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perfom_deliveries = true
+  config.action_mailer.perform_deliveries = true
 
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.sendgrid.net',

dff45f74c4d5a0e8a77a4b4b567b29e17939a5b1
Safonov88 <safonovdmitr@gmail.com>
2017-12-06 22:46:17 +0300

    validate foto_blank photo email

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,8 +4,15 @@ class Photo < ApplicationRecord
 
   validates :event, presence: true
   validates :user, presence: true
+  validate :photo_is_blank?
 
   mount_uploader :photo, PhotoUploader
 
   scope :persisted, -> {where "id IS NOT NULL"}
+
+  def photo_is_blank?
+    if photo.blank?
+      errors.add(:photo, :photo_blank)
+    end
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,13 +53,13 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = {:host => 'localhost:3000'}
-
+  #config.action_mailer.asset_host = 'http://localhost:3000'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
     user_name: 'rubyonrailstestmail',
-    
+    password: Rails.application.secrets.email_pass,
     authentication: 'plain',
     enable_starttls_auto: true
   }
